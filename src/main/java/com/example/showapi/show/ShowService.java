b2c38f5c9758dda4d5c020cef88bb2f54e359a52
Diego Fons <diego.fons@intraway.com>
2020-11-18 12:25:32 -0300

    Add caching support to shows, theaters and sections

--- src/main/java/com/example/showapi/show/ShowService.java
+++ src/main/java/com/example/showapi/show/ShowService.java
@@ -2,6 +2,7 @@ package com.example.showapi.show;
 
 import java.text.ParseException;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -9,6 +10,7 @@ import com.example.showapi.show.domain.Show;
 
 public interface ShowService {
 
+	@Cacheable("showById")
 	public Show getShowById( String id );
 	
 	public Page<Show> findAll( Pageable paging );
--- src/main/java/com/example/showapi/theater/TheaterService.java
+++ src/main/java/com/example/showapi/theater/TheaterService.java
@@ -2,6 +2,8 @@ package com.example.showapi.theater;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -13,23 +15,28 @@ import com.example.showapi.theater.request.SectionPatchRequest;
 
 public interface TheaterService {
 
+	@Cacheable("theaterById")
 	public Theater getById( String id );
 	
 	public Page<Theater> findAll( Pageable paging );
 	
 	public List<Seat> getSeatsBySection( String sectionId );
 	
+	@Cacheable("sectionById")
 	public Section getSectionById( String sectionId );
 	
 	public Page<Section> findAllSections( Pageable paging );
 	
+	@CacheEvict(cacheNames = "sectionById", allEntries = true)
 	public Boolean updateSection( String sectionId, SectionPatchRequest request );
 	
 	// New Section2.
+	@Cacheable("section2ById")
 	public Section2 getSection2ById( String sectionId );
 	
 	public Page<Section2> findAllSections2( Pageable paging );
 	
+	@CacheEvict(cacheNames = "section2ById", allEntries = true)
 	public Boolean updateSection2( String sectionId, SectionPatchRequest request );
 	
 }
